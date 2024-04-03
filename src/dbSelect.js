b997f3de821bf1b38ffbe7d687afeb425295b213
beltrame <mateus@adminfo.com.br>
2024-04-03 11:50:01 -0300

    Ajustado Update

--- src/dbSelect.js
+++ src/dbSelect.js
@@ -19,15 +19,33 @@ function buscarMensagem(empresa, tenant) {
             }
             console.log('Conexão bem-sucedida ao banco de dados!');
                         
-            const sql = 'SELECT * FROM mensagens WHERE empresa = ? AND tenant = ?';
-            connection.query(sql, [empresa, tenant], (err, result) => {
+            const sql = 'SELECT * FROM mensagens WHERE enviado = false AND empresa = ? AND tenant = ?';
+            connection.query(sql, [empresa, tenant], (err, result) => { 
                 if (err) {
                     console.error('Erro ao buscar mensagens:', err);
                     reject(err);
                     return;
                 }
                 
-                console.log('Mensagens encontradas:', result);
+                result.forEach((row) => {
+                    // Aqui você pode fazer o UPDATE usando os dados de cada linha (row) do SELECT
+                    const updateSQL = 'UPDATE mensagens SET enviado = ? WHERE codigo = ?';
+                    const coluna1NovoValor = true;
+                                    
+                    connection.query(updateSQL, [coluna1NovoValor, row.codigo], (error, results) => {
+                      if (error) {
+                        console.error('Erro ao executar o UPDATE:', error);
+                        return;
+                      }
+                      console.log('UPDATE executado com sucesso para a linha com ID:', row.codigo);
+                    });
+                  });
+
+
+
+
+
+                //console.log('Mensagens encontradas:', result);
                 
                 // Encerrar a conexão
                 connection.end();
